a917efa0ca107dd092ca22233c2f2a85bf2f5540
Jesse <ya710728@gmail.com>
2025-10-23 17:57:12 +0800

    add drama test

--- tests/otherTest4.spec.ts
+++ tests/otherTest4.spec.ts
@@ -21,3 +21,21 @@ test("drop down test", async ({ page }) => {
 
   await page.locator("select#files").selectOption({ label: "Python" });
 });
+
+test("test", async ({ page }) => {
+  await page.goto("https://gimytv.ai/");
+
+  const tvDrama = page.locator("#example-navbar-collapse ul li", {
+    hasText: "電視劇",
+  });
+
+  await tvDrama.click();
+
+  const twDrama = page.locator(".content-menu .item").getByText("台劇");
+  await twDrama.click();
+
+  const theOutLawOfDoctor = page.getByText("化外之醫");
+  await theOutLawOfDoctor.click();
+
+  await page.locator("ul#con_playlist_7 li").getByText("第01集").click();
+});
