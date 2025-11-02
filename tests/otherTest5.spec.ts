0ba716593478377070e99e54ea3bb491ba64feaf
Jesse <ya710728@gmail.com>
2025-11-02 17:38:07 +0800

    add otherTest5

---
+++ tests/otherTest5.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://testautomationpractice.blogspot.com/");
+});
+
+test("Pagination Web Table", async ({ page }) => {
+  const pageTable = page.locator("#HTML8");
+  const checkRowLatop = page
+    .locator("#productTable tbody tr", { hasText: "Laptop" })
+    .locator("td")
+    .last()
+    .locator("input");
+  await checkRowLatop.check();
+
+  const checkSmartwatch = page
+    .locator("#productTable tbody tr", { hasText: "Smartwatch" })
+    .locator("td")
+    .last()
+    .locator("input");
+  await checkSmartwatch.check();
+
+  // 選第3頁
+  await page.locator("ul#pagination li", { hasText: "3" }).click();
+  const checkRouter = page
+    .locator("#productTable tbody tr", { hasText: "Router" })
+    .locator("td")
+    .last()
+    .locator("input");
+  await checkRouter.check();
+});
+
+test("select", async ({ page }) => {
+  await page.locator("#country").selectOption("australia");
+  await page.locator("#colors").selectOption(["blue", "white", "yellow"]);
+  await page.locator("#animals").selectOption(["cat", "dog", "lion", "zebra"]);
+});
+
+test("textarea", async ({ page }) => {
+  await page
+    .getByRole("textbox", { name: "Address:" })
+    .fill("Taiwan, New Taiper City");
+
+  await page.getByRole("textbox", { name: "Enter Phone" }).fill("0912345678");
+
+  await page
+    .getByRole("textbox", { name: "Enter EMail" })
+    .fill("test@test.com");
+
+  await page.getByPlaceholder("Enter Name").fill("Ray Hao");
+});
+
+test("radio", async ({ page }) => {
+  await page.getByRole("radio", { name: "Female" }).check();
+  await page.locator(".form-check #male").check();
+});
+
+test("checkbox", async ({ page }) => {
+  await page.getByRole("checkbox", { name: "Wednesday" }).check();
+
+  await page
+    .locator(".form-group .form-check .form-check-input#thursday")
+    .check();
+  await page.getByRole("checkbox", { name: "Friday" }).check();
+  await page
+    .locator('.form-group .form-check .form-check-input[value="tuesday"]')
+    .check();
+});
